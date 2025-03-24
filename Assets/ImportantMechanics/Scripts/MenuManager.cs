eafded6041247568bdf13267d4a8dc774126e1a9
Xavier-Ickes-Student <xaicdb1@student.oaisd.org>
2025-03-24 09:48:23 -0400

    Menu stuff

--- Assets/ImportantMechanics/Scripts/MenuManager.cs
+++ Assets/ImportantMechanics/Scripts/MenuManager.cs
@@ -6,6 +6,7 @@ public class MenuManager : MonoBehaviour
 {
 
     public GameObject PauseMenu;
+    public GameObject FirstPersonController;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,7 @@ public class MenuManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseMenu.SetActive(!PauseMenu.activeSelf);
+            GetComponent<FirstPersonController>().enabled = false;
         }
     }
 }
